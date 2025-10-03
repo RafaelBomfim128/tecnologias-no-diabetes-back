988e1a8feb845fb72c06fd3d0ff8fc8f76c085fd
Rafael Assis <ra-bonfim@hotmail.com>
2025-10-02 22:44:55 -0300

    Add x-proxy-error

--- controllers/proxyController.js
+++ controllers/proxyController.js
@@ -1,28 +1,28 @@
 exports.getContentHtml = async (req, res) => {
-  const targetUrl = req.query.url;
+    const targetUrl = req.query.url;
 
-  if (!targetUrl) {
-    console.error('Erro ao devolver HTML pelo Proxy: URL ausente');
-    return res.status(400).json({ error: 'Missing url parameter' });
-  }
+    if (!targetUrl) {
+        console.error('Erro ao devolver HTML pelo Proxy: URL ausente');
+        return res.status(400).json({ error: 'Missing url parameter' });
+    }
 
-  try {
-    const response = await fetch(targetUrl, { method: 'GET' });
+    try {
+        const response = await fetch(targetUrl, { method: 'GET' });
+        const text = await response.text();
 
-    const text = await response.text();
+        res.status(response.status);
 
-    res.status(response.status);
+        const contentType = response.headers.get('content-type') || 'text/html; charset=utf-8';
+        res.set('Content-Type', contentType);
 
-    const contentType = response.headers.get('content-type') || 'text/html; charset=utf-8';
-    res.set('Content-Type', contentType);
+        res.set('Access-Control-Allow-Origin', '*');
 
-    res.set('Access-Control-Allow-Origin', '*');
+        res.send(text);
+    } catch (err) {
+        console.error('Erro no proxy ao acessar target:', err.message);
 
-    res.send(text);
-  } catch (err) {
-    console.error('Erro no proxy ao acessar target:', err.message);
-
-    res.set('Access-Control-Allow-Origin', '*');
-    res.status(502).send('Proxy error: Could not connect to URL');
-  }
+        res.set('Access-Control-Allow-Origin', '*');
+        res.set('X-Proxy-Error', '1');
+        res.status(502).send('Proxy error: não foi possível conectar ao Nightscout');
+    }
 };
